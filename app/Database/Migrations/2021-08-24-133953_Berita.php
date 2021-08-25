4d4d8d22ee64c3ab4beb5a8ef785848462aa4710
AlfarrelIndrawan <alfarrelindrawan@gmail.com>
2021-08-25 09:35:26 +0700

    migrasi

---
+++ app/Database/Migrations/2021-08-24-133953_Berita.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Database\Migrations;
+
+use CodeIgniter\Database\Migration;
+
+class Berita extends Migration
+{
+	public function up()
+	{
+		$this->forge->addField([
+            'id_berita'          => [
+                'type'           => 'INTEGER',
+                'unsigned'       => true,
+                'auto_increment' => true,
+            ],
+            'judul'       => [
+                'type'           => 'VARCHAR',
+                'constraint'     => '255',
+            ],
+            'tanggal'		=> [
+				'type'		=> 'DATE'
+			],
+			'berita'		=> [
+				'type'		=> 'TEXT'
+			],
+			'penulis'		=> [
+				'type'			=> 'VARCHAR',
+				'constraint'	=> '50',
+			],
+			'kategori'		=> [
+				'type'			=> 'ENUM',
+				'constraint'	=> "'desa', 'umkm'",
+			],
+			'foto'			=> [
+				'type'			=> 'VARCHAR',
+				'constraint'	=> '256',
+				'null'			=> 'true',
+			]
+        ]);
+        $this->forge->addPrimaryKey('id_berita');
+        $this->forge->createTable('berita');
+	}
+
+	public function down()
+	{
+		$this->forge->dropTable('Berita');
+	}
+}
