50b4a017677e42ec998be31da6021a827fb0699c
allandrp <beritaanime7@gmail.com>
2021-08-26 10:20:47 +0700

    edit halaman admin

--- app/Views/admin.php
+++ app/Views/admin.php
@@ -12,7 +12,7 @@
     <title>SB Admin 2 - Dashboard</title>
 
     <!-- Custom fonts for this template-->
-    <link href="<?= base_url('vendor/fontawesome-free/css/all.min.css') ?>" rel="stylesheet" type="text/css">
+    <link href="<?= base_url('asset/fontawesome-free/css/all.min.css') ?>" rel="stylesheet" type="text/css">
     <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
 
     <!-- Custom styles for this template-->
@@ -335,17 +335,17 @@
     </div>
 
     <!-- Bootstrap core JavaScript-->
-    <script src="<?= base_url('vendor/jquery/jquery.min.js') ?>"></script>
-    <script src="<?= base_url('vendor/bootstrap/js/bootstrap.bundle.min.js') ?>"></script>
+    <script src="<?= base_url('asset/jquery/jquery.min.js') ?>"></script>
+    <script src="<?= base_url('asset/bootstrap/js/bootstrap.bundle.min.js') ?>"></script>
 
     <!-- Core plugin JavaScript-->
-    <script src="<?= base_url('vendor/jquery-easing/jquery.easing.min.js') ?>"></script>
+    <script src="<?= base_url('asset/jquery-easing/jquery.easing.min.js') ?>"></script>
 
     <!-- Custom scripts for all pages-->
     <script src="<?= base_url('js/sb-admin-2.min.js') ?>"></script>
 
     <!-- Page level plugins -->
-    <script src="<?= base_url('vendor/chart.js/Chart.min.js') ?>"></script>
+    <script src="<?= base_url('asset/chart.js/Chart.min.js') ?>"></script>
 
     <!-- Page level custom scripts -->
     <script src="<?= base_url('js/demo/chart-area-demo.js') ?>"></script>
