b27a8a86f5705940f0571b77d6891b5032bdd4e7
AlfarrelIndrawan <alfarrelindrawan@gmail.com>
2021-09-20 13:06:30 +0700

    tambah keywords

--- app/Views/main/headerfooter.php
+++ app/Views/main/headerfooter.php
@@ -9,6 +9,7 @@ use Config\Pager;
     <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="keywords" content="Desa Plandi, Plandi, Desa, Web Desa, Desa Pemerintahan, Web Pemerintahan, Pemerintahan Indonesia, Kabupaten Malang, Desa Kabupaten Malang, Web Kabupaten Malang">
 
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
